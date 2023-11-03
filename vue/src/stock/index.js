8adc14d80f340d3663cd5fbe73fc4e633635c970
Hafiz Noor Ullah Usmani <unoor51@gmail.com>
2023-11-03 18:43:20 +0500

    First Commit

---
+++ vue/src/stock/index.js
@@ -0,0 +1,12 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+    state: {},
+    gatters:{},
+    actions: {},
+    mutations: {},
+    modules: {},
+
+})
+
+export default store;
---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+</script>
+
+<template>
+  <main>
+    <HelloWorld />
+  </main>
+</template>
